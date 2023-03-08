50d7a42dd62b5a2e4593509efda2eb9ecf80bea6
MarcusUniversee <33048697+MarcusUniversee@users.noreply.github.com>
2023-03-08 13:44:44 -0800

    new paths

--- src/main/java/frc/robot/subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision/Vision.java
@@ -8,6 +8,8 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.subsystems.DriveTrain.DriveTrain;
 
 public class Vision extends SubsystemBase {
   /** Creates a new Vision. */
@@ -20,13 +22,48 @@ public class Vision extends SubsystemBase {
     return m_instance;
   }
   private Vision() {
-    //table = NetworkTableInstance.getDefault().getTable("vision");
+    table = NetworkTableInstance.getDefault().getTable("vision");
+  }
+
+  public double[] getObjPosition() {
+    String[] sdef = {"null"};
+    Number[] def = {-1,-1};
+    double[] ddef = {0, 0};
+    if (table.getEntry("xPos").getStringArray(sdef)[0].equals("null")) {
+      return ddef;
+    }
+    double xCoord = Double.parseDouble(table.getEntry("xPos").getStringArray(sdef)[0]);
+    double distance = table.getEntry("distance").getNumberArray(def)[0].doubleValue();
+    double robotAngle = DriveTrain.getInstance().getGyroAngle();
+    double theta = Math.toRadians(VisionConstants.CAM_ANGLE) + (Math.toRadians(VisionConstants.CAM_HORIZONTAL_FOV) * (xCoord - 0.5*VisionConstants.STREAM_WIDTH_PIXELS)/(0.5*VisionConstants.STREAM_WIDTH_PIXELS));
+
+    double frontObjDist = Math.sqrt(VisionConstants.CAM_OFFSET*VisionConstants.CAM_OFFSET + distance*distance - 2*VisionConstants.CAM_OFFSET*distance*Math.cos(theta));
+    
+    double frontObjAng = Math.asin(distance*Math.sin(theta)/frontObjDist) - Math.PI/2 - Math.toRadians(robotAngle);
+    
+    double objX = frontObjDist*Math.cos(frontObjAng);
+    double objY = frontObjDist*Math.sin(frontObjAng);
+    SmartDashboard.putNumber("objrelativeX", objX);
+    
+    double rX = VisionConstants.ROBOT_RADIUS*Math.cos(Math.toRadians(robotAngle));
+    double rY = VisionConstants.ROBOT_RADIUS*Math.sin(Math.toRadians(-robotAngle));
+    
+    //this should be the final positions of the object on the field
+    double xPos = DriveTrain.getInstance().m_poseX + objX + rX;
+    double yPos = DriveTrain.getInstance().m_poseY + objY + rY;
+    double[] pos = {xPos, yPos};
+    return pos;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     Number[] def = {-1,-1};
-    //SmartDashboard.putNumber("vision", table.getEntry("distance").getNumberArray(def)[0].doubleValue());
+    String[] sdef = {"null"};
+    SmartDashboard.putNumber("vision", table.getEntry("distance").getNumberArray(def)[0].doubleValue());
+    SmartDashboard.putString("obj", table.getEntry("obj").getStringArray(sdef)[0]);
+    SmartDashboard.putString("objXPX", table.getEntry("xPos").getStringArray(sdef)[0]);
+    SmartDashboard.putNumber("objXpos", getObjPosition()[0]);
+    SmartDashboard.putNumber("objYpos", getObjPosition()[1]);
   }
 }
